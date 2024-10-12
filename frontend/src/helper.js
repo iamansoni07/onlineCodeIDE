adebd31be9e5219b5cac412c2402d1afc2a2678e
iamansoni07 <aman.soni0713@gmail.com>
2024-10-12 12:00:00 +0530

    commits

---
+++ frontend/src/helper.js
@@ -0,0 +1,9 @@
+import { useState } from "react";
+
+export const toggleClass = (el,className) => {
+  let elem = document.querySelector(el);
+  elem.classList.toggle(className);
+};
+
+
+export const api_base_url = "http://localhost:3000"

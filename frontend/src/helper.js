e803e9f3ac8ff323210cd4f4e5d8efe4b61964cf
iamansoni07 <aman.soni0713@gmail.com>
2024-10-12 12:00:00 +0530

    commits

--- frontend/src/helper.js
+++ frontend/src/helper.js
@@ -5,5 +5,9 @@ export const toggleClass = (el,className) => {
   elem.classList.toggle(className);
 };
 
+export const removeClass = (el,className) => {
+  let elem = document.querySelector(el);
+  elem.classList.remove(className);
+};
 
 export const api_base_url = "http://localhost:3000"

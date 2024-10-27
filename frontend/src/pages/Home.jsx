74a628093bf6bca0672fef77deafb3e6d8cd63ce
iamansoni07 <aman.soni0713@gmail.com>
2024-10-27 12:00:00 +0530

    commits

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from 'react'
+import Navbar from '../components/Navbar'
+import ListCard from '../components/ListCard';
+import GridCard from '../components/GridCard';
+import { api_base_url } from '../helper';
+import { useNavigate } from 'react-router-dom';
+
+const Home = () => {
+
+
+
+
+
+
+
+  useEffect(() => {
+    getProj();
+  }, []);
+
+
+  const [userData, setUserData] = useState(null);
+  const [userError, setUserError] = useState("");;
+
+
+
+  const [isGridLayout, setIsGridLayout] = useState(false);
+
+
+  return (
+    <>
+      <Navbar isGridLayout={isGridLayout} setIsGridLayout={setIsGridLayout} />
+      <div className='flex items-center justify-between px-[100px] my-[40px]'>
+        <h2 className='text-2xl'>Hi, {userData ? userData.username : ""} 👋</h2>
+        <div className='flex items-center gap-1'>
+          {/* Search Bar */}
+          <div className="inputBox !w-[350px]">
+            <input
+              type="text"
+              placeholder='Search Here... !'
+              value={searchQuery} // Bind search input to searchQuery state
+              onChange={(e) => setSearchQuery(e.target.value)} // Update searchQuery on input change
+            />
+          </div>
+          <button onClick={() => { setIsCreateModelShow(true) }} className='btnBlue rounded-[5px] mb-4 text-[20px] !p-[5px] !px-[10px]'>+</button>
+        </div>
+      </div>
+
+
+
+
+    </>
+  );
+}
+
+export default Home;

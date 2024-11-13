801b16501ca19b0e6a13680b73412d9d4681b4a2
iamansoni07 <aman.soni0713@gmail.com>
2024-11-13 12:00:00 +0530

    .

--- frontend/src/components/EditiorNavbar.jsx
+++ frontend/src/components/EditiorNavbar.jsx
@@ -7,6 +7,9 @@ const EditiorNavbar = () => {
   return (
     <>
       <div className="EditiorNavbar flex items-center justify-between px-[100px] h-[80px] bg-[#040916]">
+        <div className="logo">
+          <img className='w-[150px] cursor-pointer' src={logo} alt="" />
+        </div>
       </div>
     </>
   )

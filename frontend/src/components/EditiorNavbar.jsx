b9edf715fcf95968a399ab75ac2358dd41b3c588
iamansoni07 <aman.soni0713@gmail.com>
2024-11-13 12:00:00 +0530

    .

---
+++ frontend/src/components/EditiorNavbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import logo from "../images/logo.png"
+import { FiDownload } from "react-icons/fi";
+
+
+const EditiorNavbar = () => {
+  return (
+    <>
+      <div className="EditiorNavbar flex items-center justify-between px-[100px] h-[80px] bg-[#040916]">
+      </div>
+    </>
+  )
+}
+
+export default EditiorNavbar

14d601305152ea7b0cf8e52eec6c50b6a201b7eb
iamansoni07 <aman.soni0713@gmail.com>
2024-12-01 12:00:00 +0530

    commits

---
+++ backend/app.js
@@ -0,0 +1,44 @@
+
+
+
+
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var cors = require("cors");
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var mongoose = require('mongoose');
+
+// Replace with your actual MongoDB URI
+const uri = 'mongodb://127.0.0.1:27017/online-code-ide';
+
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log('Connected to MongoDB');
+}).catch(err => {
+  console.error('Failed to connect to MongoDB', err);
+});
+
+var app = express();
+
+
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+
+
+
+module.exports = app;

7e19279deef20977635b6303e4c82fee7d9746dc
iamansoni07 <aman.soni0713@gmail.com>
2024-12-01 12:00:00 +0530

    .

--- frontend/src/components/ListCard.jsx
+++ frontend/src/components/ListCard.jsx
@@ -8,7 +8,27 @@ const ListCard = ({item}) => {
   const navigate = useNavigate();
   const [isDeleteModelShow, setIsDeleteModelShow] = useState(false);
   
-
+  const deleteProj = (id) => {
+    fetch(api_base_url + "/deleteProject",{
+      mode: "cors",
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        progId: id,
+        userId: localStorage.getItem("userId")
+      })
+    }).then(res=>res.json()).then(data=>{
+      if(data.success){
+        setIsDeleteModelShow(false)
+        window.location.reload()
+      }else{
+        alert(data.message)
+        setIsDeleteModelShow(false)
+      }
+    })
+  }
   return (
     <>
       <div className="listCard mb-2 w-[full] flex items-center justify-between p-[10px] bg-[#040916] cursor-pointer rounded-lg hover:bg-[#111b34]">

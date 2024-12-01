58e50ff4b4ebe38f785febd4e6d9f89f59d45902
iamansoni07 <aman.soni0713@gmail.com>
2024-12-01 12:00:00 +0530

    .

--- frontend/src/components/ListCard.jsx
+++ frontend/src/components/ListCard.jsx
@@ -44,7 +44,11 @@ const ListCard = ({item}) => {
         </div>
       </div>
 
+      {
+        isDeleteModelShow ? <div className="model fixed top-0 left-0 w-screen h-screen bg-[rgba(0,0,0,0.1)] flex justify-center items-center flex-col">
 
+        </div> : ""
+      }
     </>
   )
 }

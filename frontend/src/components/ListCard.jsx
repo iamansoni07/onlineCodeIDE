515e94c8ead2ae5e9a7b7f2cc28ce922ba2ffa84
iamansoni07 <aman.soni0713@gmail.com>
2024-12-01 12:00:00 +0530

    .

---
+++ frontend/src/components/ListCard.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react'
+import img from "../images/code.png"
+import deleteImg from "../images/delete.png"
+import { api_base_url } from '../helper';
+import { useNavigate } from 'react-router-dom';
+
+const ListCard = ({item}) => {
+ 
+
+  return (
+    <>
+      <div className="listCard mb-2 w-[full] flex items-center justify-between p-[10px] bg-[#040916] cursor-pointer rounded-lg hover:bg-[#111b34]">
+
+
+      </div>
+
+
+    </>
+  )
+}
+
+export default ListCard

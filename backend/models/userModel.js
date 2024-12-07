0658dab7b46b984342b8706c3b3667d8333b6721
iamansoni07 <aman.soni0713@gmail.com>
2024-12-07 12:00:00 +0530

    commits

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -3,6 +3,10 @@ let mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/online-code-ide');
 
 let userSchema = new mongoose.Schema({
+  name: String,
+  username: String,
+  email: String,
+  password: String,
 
 
 

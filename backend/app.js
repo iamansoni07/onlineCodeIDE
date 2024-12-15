898a24ac136dcec517f71cfdb9cd21ed39cf81e7
iamansoni07 <aman.soni0713@gmail.com>
2024-12-15 12:00:00 +0530

    commits

--- backend/app.js
+++ backend/app.js
@@ -28,7 +28,9 @@ mongoose.connect(uri, {
 
 var app = express();
 
-
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());

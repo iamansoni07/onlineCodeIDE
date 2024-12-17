4f7aa0506df21fe3af664d2f212f98d841eeecc9
Aman Soni <137559453+iamansoni07@users.noreply.github.com>
2024-12-17 08:18:52 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -81,7 +81,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(cors());
+app.use(cors({
+  origin: [""],
+  methods: ["POST", "GET"],
+  credentials: true
+}));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
